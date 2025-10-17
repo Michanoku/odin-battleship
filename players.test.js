166685cc5a7a9c6923aa1b6a1671c0420811a38e
Michanoku <michael.werker@gmail.com>
2025-10-17 06:28:42 +0000

    feat: Add another test for player creation

--- players.test.js
+++ players.test.js
@@ -1,6 +1,14 @@
+import { Player } from './players.js';
 
-test('Test player creation.', () => {
+// Test player creation
+test('Player creation.', () => {
   const player = new Player('Michanoku', false);
   expect(player.name).toBe('Michanoku');
   expect(player.cpu).toBe(false);
+});
+
+test('Player creation.', () => {
+  const player = new Player('Atom', true);
+  expect(player.name).toBe('Atom');
+  expect(player.cpu).toBe(true);
 });

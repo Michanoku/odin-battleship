f446f16eb70e337de5cc2cff44dfaa42961ea6fe
Michanoku <michael.werker@gmail.com>
2025-10-17 03:30:09 +0000

    feat: Add test for ship hit

--- ships.test.js
+++ ships.test.js
@@ -8,3 +8,19 @@ for (let i = 2; i < 6; i ++) {
     expect(ship.health).toBe(i);
   });
 }
+
+// Test Ship Hit
+test('Ship hit.', () => {
+  const ship = new Ship(5);
+  const sunk = ship.hit();
+  expect(ship.health).toBe(4);
+  expect(sunk).toBe(false);
+});
+
+test('Ship hit.', () => {
+  const ship = new Ship(2);
+  const sunk1 = ship.hit();
+  const sunk2 = ship.hit();
+  expect(ship.health).toBe(0);
+  expect(sunk2).toBe(true);
+});

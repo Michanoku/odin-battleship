922e79313f4af3a8b45d45c529ac6d8c020a8dbe
Michanoku <michael.werker@gmail.com>
2025-10-30 02:16:28 +0000

    feat: Add comments to ships.js

--- src/ships.js
+++ src/ships.js
@@ -1,39 +1,16 @@
-/* Ship Class for Ship Objects:
-
-BattleShip Ships:  (Amount, Name, Size)
-1 	Carrier 	5
-2 	Battleship 	4
-3 	Destroyer 	3
-4 	Submarine 	3
-5 	Patrol Boat 	2 
-
-Methods or Variables:
-
-length (the ships length) 
-HP (same as length at first, going towards 0 if hit) 
-Description says number of hits, but going backwards from HP seems simpler
-In this thought, its a question if we need length at all....
-isSunk (should be a method checking if HP is 0)
-
-hit() function (decreases HP)
-
-
-Consideration of what methods are commnunicating outside and need testing:
-constructur() - receives the desired length of the ship and should set length and HP accordingly.
-hit() - receives the info that the ship was hit and should reduce hp by 1
-isSunk() - should probably be checked after every hit and returns the info 
-to whatever is sending the hit. (the gameboard) 
-*/
-
+// The ship class that handles ships on the map
 class Ship {
+  // The ship only saves it's own health
   constructor(length) {
     this.health = length;
   }
 
+  // On hit, lose one point
   hit() {
     this.health--;
   }
 
+  // Check if ship is sunk, if health is 0, it is sunk
   isSunk() {
     return this.health === 0;
   }
